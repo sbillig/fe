4aabebdc29458a566ef9793a170b38016c132e6d
sbillig <sean.billig@gmail.com>
2026-05-20 17:08:58 -0700

    Remove stale developer config

--- .github/install_deps.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y libboost-all-dev
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 50
-sudo update-alternatives --set g++ "/usr/bin/g++-10"
