410e3cbab0291cf489c8d4bc519e48de5d1dc404
Sean Billig <sean.billig@gmail.com>
2026-07-31 17:01:29 -0700

    fix: complete generic function call defaults

--- crates/hir/src/analysis/ty/ty_check/expr.rs
+++ crates/hir/src/analysis/ty/ty_check/expr.rs
@@ -3578,6 +3578,7 @@ impl<'db> TyChecker<'db> {
                     }
                 }
                 PathRes::Func(ty) => {
+                    let ty = self.instantiate_to_term(ty);
                     let mut callable =
                         Callable::new(self.db, ty, expr.span(self.body()).into(), None)
                             .expect("function item path should resolve to callable");
@@ -3585,7 +3586,7 @@ impl<'db> TyChecker<'db> {
                         return ExprProp::invalid(self.db);
                     }
 
-                    ExprProp::new(self.instantiate_to_term(callable.ty(self.db)), true)
+                    ExprProp::new(callable.ty(self.db), true)
                 }
                 PathRes::Trait(trait_) => {
                     let diag = BodyDiag::NotValue {
